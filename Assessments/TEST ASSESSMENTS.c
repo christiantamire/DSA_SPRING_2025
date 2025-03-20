5d45295ab47588f52ad9e2ad8319c4971321ca53
Timothy <149287561+Otim24@users.noreply.github.com>
2025-03-20 16:55:30 +0300

    Update TEST ASSESSMENTS.c

--- Assessments/TEST ASSESSMENTS.c
+++ Assessments/TEST ASSESSMENTS.c
@@ -11,21 +11,42 @@ int main()
     //Taking the inputs for Physics
     printf("Enter mark for physics (Assignments, Coure Work, Midterms, End Term): ");
     for (i = 0; i < 4; i++){
-        scanf("%f", &physics[i]);
+
+         do {
+            printf("Enter mark for physics %d: ", i + 1);
+            scanf("%f", &physics[i]);
+            if (physics[i] < 0 || physics[i] > 100) {
+                printf("Invalid input! Marks for physics should be between 0 and 100. Please try again.\n");
+            }
+        } while (physics[i] < 0 || physics[i] > 100);
         sum_physics += physics[i];
     }
 
     //Taking the input for Chemistry
      printf("Enter mark for chemistry (Assignments, Coure Work, Midterms, End Term): ");
     for (i = 0; i < 4; i++){
-        scanf("%f", &chemistry[i]);
+
+        do {
+            printf("Enter mark for chemistry %d: ", i + 1);
+            scanf("%f", &chemistry[i]);
+            if (chemistry[i] < 0 || chemistry[i] > 100) {
+                printf("Invalid input! Marks for chemistry should be between 0 and 100. Please try again.\n");
+            }
+        } while (chemistry[i] < 0 || chemistry[i] > 100);
         sum_chemistry += chemistry[i];
     }
 
     //Taking the input for Math
     printf("Enter mark for math (Assignments, Coure Work, Midterms, End Term): ");
     for (i = 0; i < 4; i++){
-        scanf("%f", &math[i]);
+
+        do {
+            printf("Enter mark for chemistry %d: ", i + 1);
+            scanf("%f", &math[i]);
+            if (math[i] < 0 || math[i] > 100) {
+                printf("Invalid input! Marks for math should be between 0 and 100. Please try again.\n");
+            }
+        } while (math[i] < 0 || math[i] > 100);
         sum_math += math[i];
     }
 
